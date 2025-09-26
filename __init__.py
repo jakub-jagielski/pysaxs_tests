d33efafe12caca0016895adc2d661b0f3a772fc1
Jakub Jagielski <jakubjagielski93@gmail.com>
2025-09-26 14:31:22 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PySAXS - Advanced SAXS Analysis Package
-
-This package provides comprehensive Small-Angle X-ray Scattering (SAXS) analysis tools,
-including an advanced GUI for interactive data analysis.
-"""
-
-__version__ = "0.1.0"
-__author__ = "PySAXS Development Team"
